9c4fc3a3f2aa0857f3827d0409aeb1df13f84d9e
Falk Schuetzenmeister <falk_email@yahoo.com>
2021-11-01 12:56:46 -0700

    handoff

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // following this extremely simple example https://developers.arcgis.com/javascript/latest/display-a-map/
 require([
+  "esri/config",
   "esri/identity/IdentityManager",
   "esri/layers/Layer",
   "esri/Map",
@@ -8,6 +9,7 @@ require([
   "esri/identity/OAuthInfo",
   "esri/views/MapView"],
   function (
+    esriConfig,
     esriId,
     Layer,
     Map,
@@ -16,11 +18,17 @@ require([
     OAuthInfo,
     MapView,
   ) {
+    // this is necessary to access generic ESRI resources without a
+    // login prompt
+    // esriConfig.portalUrl = "https://tnc.maps.argcis.com/arcgis"
+    /* esriConfig.apiKey =
+      "AAPKfadec5e5ca40451d9e7446d6dd0681afeWYBX8KLFJcTchbxr4DfFgQiomIPUkMn" +
+      "g7yzUiNVB8DUR4YclJV6MZayveosalIn"; */
     const info = new OAuthInfo({
       // Swap this ID out with registered application ID
       appId: "ephPh366b8TspMw6",
       // Uncomment the next line and update if using your own portal
-      // portalUrl: "https://<host>:<port>/arcgis"
+      portalUrl: "https://tnc.maps.arcgis.com/",
       // Uncomment the next line to prevent the user's signed in state from being shared with other apps on the same domain with the same authNamespace value.
       // authNamespace: "portal_oauth_inline",
       popup: false
@@ -70,13 +78,7 @@ require([
       constraints: { snapToZoom: false }
     });
 
-    view.ui.add("addDialog", "top-right");
     view.ui.add("logoutDialog", "top-right");
-
-    document.getElementById("addDialog").addEventListener("click", () => {
-      addProtectedLayer();
-      window.location.reload();
-    });
 
     document.getElementById("logoutDialog").addEventListener("click", () => {
       esriId.destroyCredentials();
